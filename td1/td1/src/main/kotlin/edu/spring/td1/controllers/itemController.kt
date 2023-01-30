ea95411330f55a527f2e5f1f8d576f4ad7919ab3
unknown <sacha.desquesnes@etu.unicaen.fr>
2023-01-30 09:17:51 +0100

    problèmes de whitelabel réglés

--- td1/td1/src/main/kotlin/edu/spring/td1/controllers/itemController.kt
+++ td1/td1/src/main/kotlin/edu/spring/td1/controllers/itemController.kt
@@ -2,12 +2,14 @@ package edu.spring.td1.controllers
 
 import items
 import edu.spring.td1.services.UIMessage
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
 import org.springframework.web.servlet.view.RedirectView
 
 
 @SessionAttributes("items")
+@Controller
 class itemController {
 
     @ModelAttribute("items")
@@ -36,9 +38,9 @@ class itemController {
     @PostMapping("/addNew")
     fun addNewAction(
             @ModelAttribute("nom") nom:String,
-            @SessionAttribute("items") items:HashSet<Item>,
+            @SessionAttribute("items") items:HashSet<items>,
             attrs:RedirectAttributes):RedirectView{
-        if(items.add(Item(nom))) {
+        if(items.add(items(nom))) {
             attrs.addFlashAttribute("msg", UIMessage.message("Ajout d'item", "$nom a été ajouté avec succès"))
         } else {
             attrs.addFlashAttribute("msg", UIMessage.message("Ajout d'item", "$nom est déjà dans la liste,<br>Il n'a pas été ajouté.","error","warning circle"))
--- td1/td1/src/main/kotlin/edu/spring/td1/services/UIMessage.kt
+++ td1/td1/src/main/kotlin/edu/spring/td1/services/UIMessage.kt
@@ -4,8 +4,7 @@ class UIMessage {
     class Message(var title:String,var message:String,var type:String,var icon:String)
 
     companion object{
-        fun message(title: String,message: String,type: String,icon: String)=
-                Message(title,message,type,icon)
+        fun message(title: String,message: String,type: String,icon: String)= Message(title,message,type,icon)
         fun message(title: String,message: String)=Message(title,message,"success","info circle")
     }
 }
