61e9671839e34aff83410e2455a5164f1d10f0b2
unknown <sacha.desquesnes@etu.unicaen.fr>
2023-01-30 08:28:11 +0100

    amélioration items

--- td1/td1/src/main/kotlin/edu/spring/td1/models/items.kt
+++ td1/td1/src/main/kotlin/edu/spring/td1/models/items.kt
@@ -1,23 +1,15 @@
-class Item constructor(var nom:String) {
-
+data class Item(var nom: String) {
     var evaluation:Int=0
 
     /*override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
-        val item = other as Item
         return other.nom==this.nom
-    }
+           }
 
-    override fun hashCode(): Int {
-        return nom.hashCode()
+           override fun hashCode(): Int {
+               return nom.hashCode()
     }
  */
-    override fun toString(): String {
-        return "Item(nom='$nom', evaluation=$evaluation)"
-    }
-
-
-
 
 }
